cfaa0f64219a3dd7adb110379f2f626333ef2878
Peter Kokot <peterkokot@gmail.com>
2016-05-02 16:53:37 +0200

    Fix coding style

--- src/Service.php
+++ src/Service.php
@@ -9,12 +9,32 @@ use PHPWorldWide\Stats\Collection\UserCollection;
 use PHPWorldWide\Stats\Mapper as BaseMapper;
 use Symfony\Component\Console\Helper\ProgressBar;
 
+/**
+ * Class Service.
+ */
 class Service
 {
+    /**
+     * @var Config
+     */
     private $config;
 
+    /**
+     * @var ProgressBar
+     */
     private $progress;
 
+    /**
+     * @var Mapper
+     */
+    private $mapper;
+
+    /**
+     * Service constructor.
+     *
+     * @param Config      $config
+     * @param ProgressBar $progress
+     */
     public function __construct(Config $config, ProgressBar $progress)
     {
         $this->config = $config;
@@ -22,6 +42,14 @@ class Service
         $this->mapper = new BaseMapper($this->config, $this->progress);
     }
 
+    /**
+     * Get topics collection from API data feed via mapper.
+     *
+     * @param $startDate
+     * @param $endDate
+     *
+     * @return TopicCollection
+     */
     public function getTopics($startDate, $endDate)
     {
         $topics = new TopicCollection();
@@ -30,6 +58,14 @@ class Service
         return $topics;
     }
 
+    /**
+     * Get comments collection from API data feed via mapper.
+     *
+     * @param \DateTime $startDate
+     * @param \DateTime $endDate
+     *
+     * @return CommentCollection
+     */
     public function getComments($startDate, $endDate)
     {
         $comments = new CommentCollection();
@@ -38,6 +74,14 @@ class Service
         return $comments;
     }
 
+    /**
+     * Get replies collection from API data via mapper.
+     *
+     * @param \DateTime $startDate
+     * @param \DateTime $endDate
+     *
+     * @return ReplyCollection
+     */
     public function getReplies($startDate, $endDate)
     {
         $replies = new ReplyCollection();
@@ -46,6 +90,14 @@ class Service
         return $replies;
     }
 
+    /**
+     * Get users collection from API data via mapper.
+     *
+     * @param \DateTime $startDate
+     * @param \DateTime $endDate
+     *
+     * @return UserCollection
+     */
     public function getUsers($startDate, $endDate)
     {
         $users = new UserCollection();
@@ -54,6 +106,14 @@ class Service
         return $users;
     }
 
+    /**
+     * Get the number of new users since the user's name configured in app
+     * configuration.
+     *
+     * @return int
+     *
+     * @throws \Exception
+     */
     public function getNewUsersCount()
     {
         return $this->mapper->getNewUsersCount();
