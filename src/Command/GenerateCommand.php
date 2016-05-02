f5609a34879a2805651cf2cac4efa295a9f3ad12
Peter Kokot <peterkokot@gmail.com>
2016-05-02 02:47:12 +0200

    Migrate to Twig template engine

--- src/Command/GenerateCommand.php
+++ src/Command/GenerateCommand.php
@@ -6,20 +6,45 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Helper\ProgressBar;
-use PHPWorldWide\Stats\Template;
 use PHPWorldWide\Stats\Config;
 use PHPWorldWide\Stats\Service;
+use Twig_Loader_Filesystem;
+use Twig_Environment;
+use Twig_Template;
+use Twig_Extensions_Extension_I18n;
 
 class GenerateCommand extends Command
 {
     private $config;
     private $progress;
+    /**
+     * @var Twig_Template
+     */
+    private $template;
 
+    /**
+     * Configures generate command for Console component.
+     */
     protected function configure()
     {
         try {
             $this->config = new Config(__DIR__.'/../../app/config/parameters.yml');
 
+            $twigLoader = new Twig_Loader_Filesystem(__DIR__.'/../../app/templates');
+            $twig = new Twig_Environment($twigLoader);
+            $twig->addExtension(new Twig_Extensions_Extension_I18n());
+
+            // Set language to English
+            putenv('LC_ALL=en_US');
+            setlocale(LC_ALL, 'en_US');
+
+            // Specify the location of the translation tables
+            bindtextdomain('myAppPhp', 'includes/locale');
+            bind_textdomain_codeset('myAppPhp', 'UTF-8');
+
+            // Choose domain
+            textdomain('myAppPhp');
+            $this->template = $twig->loadTemplate('report.html.twig');
             $this
                 ->setName('generate_command')
                 ->setDescription('Generates Facebook group report')
@@ -52,33 +77,27 @@ class GenerateCommand extends Command
             $this->progress->advance();
             $blockedCount = $this->config->get('new_blocked_count') - $this->config->get('last_blocked_count');
 
-            $data = [
-                'startDate' => $startDate->format('Y-m-d'),
-                'endDate' => $endDate->format('Y-m-d'),
-                'newUsersCount' => $service->getNewUsersCount(),
-                'topUsersCount' => $this->config->get('top_users_count'),
-                'topUsers' => $users->getTopUsers($this->config->get('top_users_count')),
-                'bannedCount' => $blockedCount,
-                'newTopicsCount' => $topics->getNewTopicsCount(),
-                'newCommentsCount' => $comments->count(),
-                'newRepliesCount' => $replies->count(),
-                'activeUsersCount' => $users->count(),
-                'mostLikesCount' => $topics->getMostLikedTopic()->getLikesCount(),
-                'mostLikedTopicId' => $topics->getMostLikedTopic()->getReportId(),
-                'mostCommentsCount' => $topics->getMostActiveTopic()->getCommentsCount(),
-                'mostActiveTopicId' => $topics->getMostActiveTopic()->getReportId(),
-                'commitsCount' => 3,
-                'topTopics' => $this->config->get('top_topics'),
-            ];
-
-            $template = new Template(__DIR__.'/../../app/templates/report.php');
-            foreach ($data as $key => $value) {
-                $template->$key = $value;
-            }
-
             $this->progress->finish();
+            $output->writeln("\n");
 
-            $output->writeln($template->render());
+            $output->writeln($this->template->render([
+                'start_date' => $startDate->format('Y-m-d'),
+                'end_date' => $endDate->format('Y-m-d'),
+                'new_users_count' => $service->getNewUsersCount(),
+                'top_users_count' => $this->config->get('top_users_count'),
+                'top_users' => $users->getTopUsers($this->config->get('top_users_count')),
+                'new_topics_count' => $topics->getNewTopicsCount(),
+                'new_comments_count' => $comments->count(),
+                'new_replies_count' => $replies->count(),
+                'active_users_count' => $users->count(),
+                'banned_count' => $blockedCount,
+                'most_likes_count' => $topics->getMostLikedTopic()->getLikesCount(),
+                'most_liked_topic_id' => $topics->getMostLikedTopic()->getReportId(),
+                'most_comments_count' => $topics->getMostActiveTopic()->getCommentsCount(),
+                'most_active_topic_id' => $topics->getMostActiveTopic()->getReportId(),
+                'commits_count' => 3,
+                'top_topics' => $this->config->get('top_topics'),
+            ]));
         } catch (\Exception $e) {
             $output->writeln($e->getMessage());
         }
